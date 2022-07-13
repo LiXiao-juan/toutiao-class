f0aaf29f1b9e6e1a51bcf478290777ff46f6273b
Li <1627843848@qq.com>
2022-07-13 12:15:55 +0800

    完成初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/fonts/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入rem适配
+import 'amfe-flexible/index.min.js'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
